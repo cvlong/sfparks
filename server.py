72597d27b9c781ac7644390beb76ef39a33e12bf
Christina Long <christinalong@Nancys-MacBook-Air-2.local>
2016-05-15 21:40:56 -0700

    updated park query functions

--- server.py
+++ server.py
@@ -4,8 +4,8 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from model import db, connect_to_db, User, Popos
+from mappingfunc import get_routing_directions, find_distance
 
-# from geocoding import [FUNCTION NAMES]
 
 app = Flask(__name__)
 
@@ -28,28 +28,64 @@ def index():
 
 
 
-@app.route('/query')
+@app.route('/query', methods="GET")
 def query_parks():
     """"""
-    # Query for park objects
+    
+    origin = request.args.get("origin")
+
+    # routing_profile = 'walking'
+
+    geocode_origin = (geocode_location(origin))
+    print geocode_origin
+    # (37.792458 -122.395709)
+
+    # hardcoding origin for testing (lat, lon tuple)
+    origin = (37.792458, -122.395709)
+
     parks = Popos.query.all()
-    print parks
+    close_parks = {}
+
+    for park in parks:
+        dist = find_distance(origin, (park.latitude, park.longitude))
+        if dist < .5: # later bring in dictionary that corresponds to bounding box heuristic
+            close_parks[park.popos_id] = park, dist
+
+    print close_parks
+
+    return render_template('query.html',
+                            close_parks=close_parks)
+
+"""
+#run server in interactive mode
+# python -i server.py
+# control C (just once)
+# stops the script
+# then can call functions
+"""
+
+
+    # parks = Popos.query.all()
+    # print parks
     
-    origins_list = []
+    # destination_list = []
     
-    # 
-    for item in parks:
-        origins_list.append(create_geojson_object(item))
-        print origins_list
+    # for item in parks:
+    #     geojson = item.create_geojson_object
+    #     destination_list.append(geojson)
+    #     print destination_list
+
+    # return destination_list
 
     # Query and get the parks
     # Get back LIST of park objects
     # Then loop thourgh that LIST
     # And add geojson_iobjs to another list, or something else
 
-    
 
-@app.route('/login', methods=['POST', 'GET'])
+
+
+@app.route('/login', methods=['POST']) #note: took out 'GET' method
 def login():
     """Show login form."""
 
@@ -65,9 +101,11 @@ def process_login():
     password = request.form.get("password")
 
     # select the user from the database who has the given email (if any)
-    user = User.query.filter_by(email=email).first()
+    user = User.query.filter(User.email=email).first()
 
     if user == None:
+        flash("That email address does not appear in our records. Creating new record for {}.".format(email))
+
         # instantiate a user object with the information provided
         user = User(email=email, password=password)
         
@@ -75,8 +113,8 @@ def process_login():
         db.session.add(user)
         db.session.commit()
 
-        # set a cookie identifying the user; return to homepage
-        session['user_id'] = user.user_id
+        # add user to the session; redirect to homepage
+        session['user'] = user.user_id
         
         flash("You're logged in.")
         return redirect('/')
@@ -84,8 +122,7 @@ def process_login():
     else:
         # check to see if password is correct
         if password == user.password:
-            # set a cookie identifying the user; return to homepage
-            session['user_id'] = user.user_id
+            session['user'] = user.user_id
             
             flash("You're logged in.")
             return redirect('/')
@@ -93,7 +130,7 @@ def process_login():
         else:  # if password does not match database
             # flash message, stay on page
             
-            flash('Your password did not match. Please enter your information again.')
+            flash('Your password is incorrect. Please enter your information again.')
             return redirect('/login')
 
 
@@ -102,7 +139,7 @@ def process_logout():
     """Log user out."""
 
     # remove user id from session
-    del session['user_id']
+    del session['user']
     
     flash('You are now logged out.')
     return redirect('/')
