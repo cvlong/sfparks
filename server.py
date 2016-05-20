1ae42eb761e79616b3ee6857a5a1f2fb93f1d212
cvlong <cvlong@gmail.com>
2016-05-19 18:51:45 -0700

    updated and refactored query functions

--- server.py
+++ server.py
@@ -3,10 +3,11 @@
 from flask import Flask, render_template, redirect, request, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
-from geojson import Feature, Point
+import json
+from geojson import Feature, Point, FeatureCollection
 from model import db, connect_to_db, User, Popos, Posm
-from geofunctions import geocode_location, reverse_coord, get_routing_times, geojson_origin_object
-from mappingfunctions import find_close_parks, make_geojson_destinations, add_routing_time
+from geofunctions import geocode_location, get_routing_times
+from mappingfunctions import find_close_parks, add_routing_time, make_feature_coll
 
 
 app = Flask(__name__)
@@ -25,110 +26,90 @@ def index():
     popos = Popos.query.all()
     posm = Posm.query.all()
 
+    popos = make_feature_coll(popos)
+    posm = make_feature_coll(posm)
+
     return render_template('homepage.html',
                             popos=popos,
                             posm=posm)
 
 
+# @app.route('/popos.json')
+# def popos_info():
+#     """GeoJSON info about popos."""
+
+#     popos = Popos.query.all()
+
+#     geojson_popos = []
+
+#     for park in popos:
+#         geojson_popos.append(park.create_geojson_object())
+
+#     popos_geo = FeatureCollection(geojson_popos)
+
+#     return jsonify(popos_geo)
+
+
 @app.route('/query', methods=['GET'])
 def query_parks():
-    """Return results from users' query based on origin location, time, and routing profile."""
+    """Return results from users' query based on origin location, time profile, and routing profile."""
     
     origin = request.args.get('origin')
-    routing_time = request.args.get('routing_time')
-    routing_profile = 'walking' # hardcoding this for now; TODO: incorporate into query params
+    time = request.args.get('time')
+    routing = 'walking' # hardcoding for now
+                        # TODO: incorporate routing into query params
 
-    # Geocode origin address input to lat/lng tuple
-    geocoded_origin = geocode_location(origin)
-        # (37.792458, -122.395709)
+    # Geocode user input; origin is an instance of named tuple
+    origin = geocode_location(origin)
 
     # Get all park objects in database
     parks = Popos.query.all()
 
-    # Instantiate a dictionary containing park objects within bounding radius heuristic
-    close_parks = find_close_parks(geocoded_origin, parks)
+    # Create a dictionary containing park objects within the bounding radius heuristic
+    close_parks = find_close_parks(origin, time, routing, parks)
 
-    # Instantiate a list of GeoJSON objects for parks in close_parks dictionary
-    geojson_destinations = make_geojson_destinations(close_parks)
+    # Create a list of GeoJSON objects for close parks
+    geojson_destinations = [park.create_geojson_object() for park in close_parks.values()]
+        # [{'geometry': {'type': 'Point', 'coordinates': [-122.40487, 37.79277]}, 'type': 'Feature', 'properties': {'name': u'600 California St', 'address': u'600 California St'}}, {'geometry': {'type': 'Point', 'coordinates': [-122.40652, 37.78473]}, 'type': 'Feature', 'properties': {'name': u'Westfield Sky Terrace (Wesfield Center Mall)', 'address': u'845 Market St'}},
 
-    # Convert geocoded_origin tuple to GeoJSON object; need to switch lat/lng to lng/lat tuple
-    geojson_origin = Feature(geometry=Point(reverse_coord(geocoded_origin)))
+    # Convert origin coordinates to GeoJSON object
+    geojson_origin = Feature(geometry=Point((origin.longitude, origin.latitude)))
         # Point(reverse_coord(geocoded_origin)) --> Feature(geometry=geojson_origin)
 
-    # Instantiate a list of GeoJSON objects for the get_routing_times function
+    # Create list of GeoJSON objects for get_routing_times argument
     routing_params = [geojson_origin] + geojson_destinations
+        # TODO .insert to list
+        # Can these be put in as two separate lists?
     
-    routing_times = get_routing_times(routing_params, routing_profile)[0][1:]
-    # routing_times = list of time in seconds from origin to each destination in geojson_destinations
+    routing_times = get_routing_times(routing_params, routing)
+    # distance matrix (in seconds)
 
-
+    # Create markers for the results of the user's query by adding each routing time to a park's GeoJSON properties
     markers = add_routing_time(geojson_destinations, routing_times)
 
+    markers = json.dumps(FeatureCollection(markers))
 
-        
-
-    #Loop through geojson list
-    #edit to add travel time parameter
-    #new geojson list that contains travel time param
-
-
+    print type(markers)
 
     return render_template('query.html',
                             origin=origin,
-                            routing_time=routing_time,
-                            close_parks=close_parks,
+                            time=time,
+                            # close_parks=close_parks,
+                            geojson_origin=geojson_origin,
                             geojson_destinations=geojson_destinations,
-                            routing_times=routing_times,
+                            # routing_times=routing_times,
                             markers=markers)
 
-
-    # print len(geojson_destinations) #8 - EIGHT destination items
-    # print len(routing_time) #9 - ignore the first, which is origin
-
-"""
-[[0, 704.3, 638.5, 720.5, 687.9, 856.7, 582.1, 586.3, 617.2],
- 
+    
 
 
 
- index0     [704.3, 0, 744, 480, 16.4, 226, 261.6, 340.6, 433.6],
- index1     [638.5, 744, 0, 470.6, 738.7, 784.9, 562.3, 485.9, 348.5],
- index2     [720.5, 480, 470.6, 0, 496.4, 376.2, 298.3, 221.9, 178.4],
- index3     [687.9, 16.4, 738.7, 496.4, 0, 242.4, 278, 357, 450],
- index4     [856.6, 225.9, 784.8, 376.1, 242.3, 0, 356.9, 435.9, 492.6],
- index5     [582, 261.5, 562.2, 298.2, 277.9, 356.9, 0, 158.8, 251.8],
- index6     [586.2, 340.5, 485.8, 221.8, 356.9, 435.9, 158.8, 0, 175.4],
- index7     [617.2, 433.6, 348.5, 178.4, 450, 492.7, 251.9, 175.5, 0]]
-"""
 
-
-
-"""
 #run server in interactive mode
 # python -i server.py
 # control C (just once)
 # stops the script
 # then can call functions
-"""
-
-
-    # parks = Popos.query.all()
-    # print parks
-    
-    # destination_list = []
-    
-    # for item in parks:
-    #     geojson = item.create_geojson_object
-    #     destination_list.append(geojson)
-    #     print destination_list
-
-    # return destination_list
-
-    # Query and get the parks
-    # Get back LIST of park objects
-    # Then loop thourgh that LIST
-    # And add geojson_objs to another list, or something else
-
 
 
 
