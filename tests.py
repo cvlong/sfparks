36f77bf0a027a72b19bc964d0fc4b9a3635a3055
cvlong <cvlong@gmail.com>
2016-06-07 19:37:19 -0700

    start testing

--- tests.py
+++ tests.py
@@ -1,24 +1,25 @@
 import unittest
+# from unittest import TestCase
 import doctest
+import server
+# from server import app
 import mappingfunctions
-# import server
 
 
-# def load_tests(loader, tests, ignore):
-#     """Includes doctests and file-based doctests.
+def load_tests(loader, tests, ignore):
+    """Includes doctests and file-based doctests.
 
-#     Note: function name, 'load_tests', is required.
-#     """
+    Note: function name, 'load_tests', is required.
+    """
 
-#     tests.addTests(doctest.DocTestSuite(server))
-#     tests.addTests(doctest.DocFileSuite("tests.txt"))
-#     return tests
+    tests.addTests(doctest.DocTestSuite(server))
+    tests.addTests(doctest.DocFileSuite("tests.txt"))
+    return tests
 
 
 class MyAppUnitTestCase(unittest.TestCase):
     """Unit tests: discrete code testing."""
 
-#ADD SETUP/TEARDOWN
     def setUp(self):
         print "(setUp ran)"
         self.client = server.app.test_client()
@@ -34,8 +35,8 @@ class MyAppUnitTestCase(unittest.TestCase):
     def test_format_origin(self):
         pass
 
-        response = mappingfunctions.format_origin(
-        self.assert
+        # response = mappingfunctions.format_origin(
+        # self.assert
 
     def test_find_appx_dist(self):
 
@@ -58,52 +59,158 @@ class MyAppUnitTestCase(unittest.TestCase):
     def test_add_routing_time(self):
         pass
 
-    def test_round_num(self):
-
-        response = mappingfunctions.round_num(1.3)
-        self.assertEqual(response, 1.5)
+    def test_format_routing_time(self):
+        pass
+        # response = mappingfunctions.format_routing_time(1.3)
+        # self.assertEqual(response, 1.5)
        
-        response = mappingfunctions.round_num(3.0)
-        self.assertEqual(response, 3.0)
+        # response = mappingfunctions.format_routing_time(3.0)
+        # self.assertEqual(response, 3.0)
 
-        response = mappingfunctions.round_num(4.1)
-        self.assertEqual(response, 4.0)
+        # response = mappingfunctions.format_routing_time(4.1)
+        # self.assertEqual(response, 4.0)
 
+## THIS IS THE SAME AS WHA'S BELOW: FlaskTestBasic
+# class MyAppIntegrationTestCase(unittest.TestCase):
+#     """Integration tests: testing Flask server."""
 
-class MyAppIntegrationTestCase(unittest.TestCase):
-    """Integration tests: testing Flask server."""
+#     def setUp(self):
+#         print "(setUp ran)"
+#         self.client = server.app.test_client()
+#         server.app.config['TESTING'] = True
+#         server.app.config['DEBUG'] = False
+
+#     def tearDown(self):
+#         # Don't need to define this method.
+#         print "(tearDown ran)"
+
+#     def test_homepage(self):
+#         result = self.client.get('/')
+#         self.assertEqual(result.status_code, 200)
+#         self.assertIn('Search SFparks', result.data)
+
+#     # def test_about_page(self):
+#     #     test_client = server.app.test_client()
+#     #     result = test_client.post('/about')
+#     #     self.assertIn('Flask', result.data)
+
+#     def test_query(self):
+#         test_client = server.app.test_client()
+#         result = test_client.post('/query?origin=555+Market+St&time=5&routing=walking')
+#         self.assertIn('100 Pine Street', result.data)
+
+class FlaskTestsBasic(unittest.TestCase):
+    """Flask tests."""
 
     def setUp(self):
-        print "(setUp ran)"
-        self.client = server.app.test_client()
-        server.app.config['TESTING'] = True
-        server.app.config['DEBUG'] = False
+        """Run before every Flask test."""
 
-    def tearDown(self):
-        # Don't need to define this method.
-        print "(tearDown ran)"
+
+        app.config['TESTING'] = True # Show Flask errors that happen during tests
+        self.client = app.test_client() # Get the Flask test client
 
     def test_homepage(self):
-        result = self.client.get('/')
+        """Test homepage page."""
+
+        result = self.client.get("/")
+        self.assertIn('<h1>HELLO</h1>', result.data)
+    
+    def test_login(self):
+        
+        result = self.client.post("/login")
         self.assertEqual(result.status_code, 200)
-        self.assertIn('Starting location', result.data)
-
-    # # TODO: Change
-    # def test_favorite_color_form(self):
-    #     test_client = server.app.test_client()
-    #     result = test_client.post('/fav_color', data={'color': 'blue'})
-    #     self.assertIn('I like blue, too', result.data)
+        self.assertIn("<h3>Sign in</h3>", result.data)
 
 
-    # TODO: Change
-    # def test_adder(self):
-    #     result = self.client.get('/add-things?x=-1&y=1')
-    #     self.assertEqual(result.data, "99")
+class FlaskTestsDatabase(TestCase):
+    """Flask tests that use the database."""
+    
+    #Create fake db in model and use that here
+    #In model.py, connect_to_db need to set additional parameter
+    #Use this to test queries from the db in server.py
 
-    # TODO: Change
-    # def test_query(self):
-    #     result = self.client.get('/')
-    #     self.assertIn('<h1>Test</h1>', result.data)
+    def setUp(self):
+        """Run before every test."""
+        
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+        # Connect to test database
+        connect_to_db(app, "postgresql:///testdb")
+
+        # Create tables and add sample data
+        db.create_all()
+        example_data()
+
+    def tearDown(self):
+        """Run at the end of every test."""
+
+        db.session.close()
+        db.drop_all()
+
+
+    def test_parks(self):
+        """Test park features."""
+
+        result = self.client.get('/current-location.json')
+        self.assertIn('', result.data)
+
+    TODO: Change
+    def test_departments_list(self):
+        """Test departments page."""
+
+        result = self.client.get("/departments")
+        self.assertIn("Legal", result.data)
+
+
+    TODO: Change
+    def test_departments_details(self):
+        """Test departments page."""
+
+        result = self.client.get("/department/fin")
+        self.assertIn("Phone: 555-1000", result.data)
+
+
+    def test_login(self):
+        """Test login page."""
+
+        result = self.client.post("/login", 
+                                  data={"user_id": "cvlong@gmail.com",
+                                        "password": "123"},
+                                  follow_redirects=True)
+        self.assertIn("You're logged in", result.data)
+
+
+class FlaskTestsLoggedIn(unittest.TestCase):
+    """Flask tests with user logged in to session."""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        server.app.config['TESTING'] = True
+        server.app.config['SECRET_KEY'] = 'key'
+        self.client = server.app.test_client()
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess['user_id'] = 1
+
+
+    def test_homepage(self):
+        """Test login page."""
+
+        result = self.client.get("/")
+        self.assertIn("Sign out", result.data)
+        self.assertNotIn("Sign in", result.data)
+
+    def test_query_page(self):
+        """Test query page."""
+
+        data = "/query?origin=555+Market+St&time=5&routing=walking"
+
+        result = self.client.get(data)
+        self.assertIn("geojson_origin", result.data)
+        self.assertIn("My Favorites", result.data)
 
 
 if __name__ == '__main__':
