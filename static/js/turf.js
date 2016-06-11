7b87e6148df9ca73d7dd4305cbdc6cb7976df59a
cvlong <cvlong@gmail.com>
2016-06-11 10:21:50 -0700

    change ajax functions

--- static/js/turf.js
+++ static/js/turf.js
@@ -1,43 +1,36 @@
 "use strict";
 
-
-
 map.on('load', function () {
     // Add park marker data from previously defined GeoJSON source.
     map.addSource("park_markers", park_markers_source);
 
-    // // Add turf data.
-    // map.addSource('turf-data', {
-    //     "type": "geojson",
-    //     "data": grid
-    // });
+    // Add turf data
+    map.addSource('turf-data', {
+        "type": "geojson",
+        "data": grid
+    });
 
-    // // Add turf.
-    // map.addLayer({
-    //     "id": "turf-data",
-    //     "type": "circle",
-    //     "source": "turf-data",
-    //     "paint": {
-    //         "circle-color": "#f00",
-    //         "circle-radius": 5
-    //     }
-    // });
+    map.addLayer({
+        "id": "turf-data",
+        "type": "circle",
+        "source": "turf-data",
+        "paint": {
+            "circle-color": "#f00",
+            "circle-radius": 5
+        }
+    });
 
-    // grid.features.forEach(function (feature) {
-    //     feature.properties["marker-color"] = "#6BC65F";
-    //     feature.properties["marker-size"] = "small";
-    // });
-
-
-
-
+    grid.features.forEach(function (feature) {
+        feature.properties["marker-color"] = "#6BC65F";
+        feature.properties["marker-size"] = "small";
+    });
 
 // Create a bounding box ('envelope') around the markers
 var enveloped = turf.envelope(park_markers);
 
 // var resultFeatures = fc.features.concat(enveloped);
 
-var result = { //put the resulting envelope in a geojson format FeatureCollection
+var result = { //put resulting envelope in geojson FeatureCollection format
     "type": "FeatureCollection",
     "features": [enveloped] 
 };
@@ -48,8 +41,7 @@ var result = { //put the resulting envelope in a geojson format FeatureCollectio
 //              enveloped.geometry.coordinates[0][2][1]]
 //             );
 
-// Use turf.point-grid 
-// has to be in order [minX, minY, maxX, maxY]
+// Use turf.point-grid [in order minX, minY, maxX, maxY]
 // TODO: Make this the extent of the entire city
 var extent = [enveloped.geometry.coordinates[0][0][0],
              enveloped.geometry.coordinates[0][0][1],
@@ -79,30 +71,10 @@ var coordArray = grid.features.map(function(point) {
 console.log(coordArray)
 
 
-// console.log(gridPoints)
-// Returns an array of comma joined coordinates. Then join with semicolons to add
-// to the string
-// ["-122.41819743,37.78274031", "-122.41819743,37.783102025779755", etc.
-
-                // $.post('/grid-distances.json', {'fc': fc}, gridDistanceSuccess);
-
-
-                // var gridDistances
-
-                // function gridDistanceSuccess(result) {
-                //     // console.log(result.status);
-                //     console.log(result)
-                //     gridDistances = result;
-                // }
-
-                // console.log(gridDistances)
-
-
-
 // Build url for API call to request distance from origin to each point in the grid
 // var distanceUrl = 'https://api.mapbox.com/distances/v1/mapbox/{{routing}}/?access_token=' + mapboxgl.accessToken;
 
-// couldinitialize an empty variable
+// could initialize an empty variable
 var points;
 
 $.ajax({
@@ -125,7 +97,7 @@ $.ajax({
 
 // .map(function(f) {return f.geometry.coordinates; })
 
-//in query string, add geometies = geojson (then you don't have to deal with the polyline module)
+// In query string, add geometies as geojson (then don't have to deal with the polyline module)
 
 $.ajax({
     type: 'GET',
@@ -136,43 +108,5 @@ $.ajax({
     error: (e) => {
         console.error(e);
     }
-})
-
-
-
-
-// takes two waypoints, and uses that to make a directions requestuild string 
-// /'coordstring'
-// ' + 'mapbox.walking' + '/' + coords.map(function(c) { c[0] + ',' + c[1]; ' }).join(';')  
-// #map iterates over the array and returns a version of it
-
-// var coordstring = '';
-// for (var i = 0; i < coords.length; i++) {
-//     coordstring += coords[i][0] + coords[i][1]
-// }
-
-// distance API is a post request
-// so look at jquery post request documentation to structure this
-// .post()
-// probably need to set header:
-// type:post
-// url:url, etc.
-// content type - see example request
-
-
-
-
-
-//build url to request distance to each point in the grid
-
-//feature collection.features = array of features
-//fc.features.map(function(feature) {return feature.geometry.coordinates.join(',')}
-//iterating over this array. transforms each object in the array from a feature to a string of two coordinates
-//mao over ffeatures since it's an array
-//that will be an array of comma joined coordinates, then join with semicolons to add to the string
-
-// still save the grid as the fc
-// want to use the center coordinate, then add the descrinations
-//so for objects in the fc (remember they'll be off by one) then add this as a property to each geojson feature
-
+});
 
