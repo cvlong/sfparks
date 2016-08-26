3a1615c6fd2045971d6f7833baa6cec581084d2a
Christina <cvlong@gmail.com>
2016-08-25 14:59:15 -1000

    Remove sys logging

--- server.py
+++ server.py
@@ -301,6 +301,6 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     # DebugToolbarExtension(app)
 
-    # PORT = int(os.environ.get("PORT", 5000))
-    # DEBUG = "NO_DEBUG" not in os.environ
+    PORT = int(os.environ.get("PORT", 5000))
+    DEBUG = "NO_DEBUG" not in os.environ
     app.run(host="0.0.0.0", port=PORT, debug=DEBUG)
